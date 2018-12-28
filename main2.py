2d25636ac35c7ce900755df3865b002547019ea4
k7ko <pkikomeko1@gmail.com>
2018-12-28 03:02:26 +0300

    Adding files

---
+++ main2.py
@@ -0,0 +1,67 @@
+from flask import Flask, jsonify, request
+
+from models import User
+from models import Incident
+import random
+import datetime
+
+incidents=[]
+
+app = Flask(__name__)
+
+
+#Get
+@app.route('/api/v1/red-flags', methods=['GET'])
+def all_redflags():
+#convert each flag in incidents list to json
+
+    allflags = [flag.to_json() for flag in incidents]
+    response ={
+        'status':200,
+        'data': allflags
+    }
+    if len(allflags) < 1:
+        return jsonify({'message': 'No incidents yet'}), 400
+    return jsonify(response)
+
+
+#Post
+@app.route('/api/v1/red-flags', methods=['POST'])
+def save_redflag():
+#convert input data to json
+    data = request.get_json()
+    print(data)
+    if not data:
+        response ={
+            'status': 400,
+                    }
+        return jsonify(response), 400
+
+    postedData = ['id', 'createdOn', 'createdBy', 'type', 'location', 'status', 'Images', 'Videos', 'comment']
+    if not all(item in data for item in postedData):
+        response = {
+            'message': 'Missing parameter(s)',
+            'status' : 400
+        }
+        return jsonify(response), 400
+
+    incident = Incident(data['id'], data['createdOn'], data['createdBy'], data['type'], data['location'], data['status'], data['Images'], data['Videos'], data['comment'])
+    incidents.append(incident)
+    response = {
+            'status' : 201,
+            'message': 'Successfully posted',
+            'data': incident.__dict__
+        }
+    return jsonify(response), 201
+
+
+
+    
+
+
+    
+        
+
+
+if __name__=='__main__':
+    app.run(host="0.0.0.0", port="5000", debug=True)
